ff77c6c7c004c1d88a660f485ea81e6d4a124b34
Kevin Cen <kevincen27@gmail.com>
2020-06-01 15:19:29 -0400

    movement of the character elf

---
+++ Assets/Character_Elf/HumanController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanController : MonoBehaviour
+{
+    public Animator anim;
+    public float moveSpeed;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 1f;
+        anim = GetComponent<Animator>();
+   
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        if (Input.GetKey("left"))
+        {
+            anim.SetBool("isRunning", true);
+
+        }
+        else if (Input.GetKey("right"))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else if (Input.GetKey("up"))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else if (Input.GetKey("down"))
+        {
+            anim.SetBool("runBack", true);
+        }
+        else { anim.SetBool("isRunning", false);
+            anim.SetBool("runBack", false);
+        }
+        
+    }
+}
---
+++ Assets/scripts/SpiderController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 1f;
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+
+
+        if (Input.GetKey("left"))
+        {
+            anim.SetBool("isRunning", true);
+
+        }
+        else if (Input.GetKey("right"))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else if (Input.GetKey("up"))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else if (Input.GetKey("down"))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else { anim.SetBool("isRunning", false); }
+
+
+        if (Input.GetKey("j"))
+        { anim.SetBool("isAttacking", true); }
+        else { anim.SetBool("isAttacking", false); }
+
+
+
+    }
+}
+
+
+
+
+        
