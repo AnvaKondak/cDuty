ae58c99ebbb9eb906553cf4a759dd505f6732041
Kevin Cen <kevincen27@gmail.com>
2020-06-02 15:54:25 -0400

    changes

--- Assets/scripts/HumanController.cs
+++ Assets/scripts/HumanController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,20 +8,21 @@ public class HumanController : MonoBehaviour
 {
     public Animator anim;
     public float moveSpeed;
-  
 
+   
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 1f;
         anim = GetComponent<Animator>();
-   
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+    
+
         transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
         if (Input.GetKey("left") || Input.GetKey("a"))
         {
@@ -39,17 +41,33 @@ public class HumanController : MonoBehaviour
         {
             anim.SetBool("runBack", true);
         }
-       else if (Input.GetKey("left") && Input.GetKey("j"))
+     /*  else if (Input.GetKey("left") || Input.GetKey("j"))
         {
             anim.SetBool("isRunning", true);
 
-        }
+        }*/
         else { anim.SetBool("isWalking", false);
             anim.SetBool("runBack", false);
             anim.SetBool("isRunning", false);
         }
-  
-      
 
+       /* if ((GetButtonDown("w") && GetButton("j"))
+    || (GetButtonDown("j") && GetButton("w"))
+   { anim.SetBool("isRunning", true); }
+   */
+
+
+
+
+        }
+
+    /*private bool GetButtonDown(string v)
+    {
+        throw new NotImplementedException();
     }
+
+    private bool GetButton(string v)
+    {
+        throw new NotImplementedException();
+    }*/
 }
