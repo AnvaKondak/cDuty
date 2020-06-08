321df88658f982a851516ffff870af5cf54d4d61
Kevin Cen <kevincen27@gmail.com>
2020-06-07 21:52:20 -0400

    changes for NPC

--- Assets/scripts/CactusController.cs
+++ Assets/scripts/CactusController.cs
@@ -2,34 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderController : MonoBehaviour
+public class CactusController : MonoBehaviour
 {
-   
 
     public Animator anim;
 
     public Transform target;
-   
-
     // Start is called before the first frame update
     void Start()
     {
-       
-
         anim = GetComponent<Animator>();
-      
     }
 
     // Update is called once per frame
     void Update()
-    {   Vector3 direction = target.position - this.transform.position;
+    {
+        Vector3 direction = target.position - this.transform.position;
         //the float angle is resposible for the area where the spider can see
         //the player that will start the chasing.↓↓
         float angle = Vector3.Angle(direction, this.transform.forward);
 
-        if (Vector3.Distance(target.position, transform.position) < 7 && angle < 30)
+        if (Vector3.Distance(target.position, transform.position) < 5 && angle < 30)
         {
-            
+
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
                                      Quaternion.LookRotation(direction), 0.1f);
 
@@ -39,37 +34,21 @@ public class SpiderController : MonoBehaviour
             if (direction.magnitude > 2)
             {
                 this.transform.Translate(0, 0, 0.05f);
-                anim.SetBool("isRunning", true);
+                anim.SetBool("isWalking", true);
                 anim.SetBool("isAttacking", false);
             }
             else
             {
-                anim.SetBool("isRunning", false);
+                anim.SetBool("isWalking", false);
                 anim.SetBool("isAttacking", true);
             }
 
         }
-        else { anim.SetBool("isIdle", true);
-            anim.SetBool("isRunning", false);
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
             anim.SetBool("isAttacking", false);
         }
-
-
-
-
-
-
-
-
-
-   
     }
-
-     
-
 }
-
-
-
-
-        
